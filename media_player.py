8803ec22a3bd172d32a213680689c9d79e1b0cc4
Maor <maorcc@users.noreply.github.com>
2021-11-17 00:32:48 +0200

    Fix exception handling when connection fails

--- media_player.py
+++ media_player.py
@@ -136,7 +136,7 @@ class PioneerDevice(MediaPlayerEntity):
                     _LOGGER.info("Connected to %s:%d", self._host, self._port)
                 except:
                     _LOGGER.error("No connection to %s:%d, retry in 30s", \
-                        self.ip, self.port)
+                        self._host, self.port)
                     await asyncio.sleep(30)
                     continue
 
